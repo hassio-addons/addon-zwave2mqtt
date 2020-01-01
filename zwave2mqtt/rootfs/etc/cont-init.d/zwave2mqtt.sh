15026310d6aa9ae511eab5c71f1497855fbc59b0
Lennard Westerveld <mail@lennard.pro>
2020-01-01 22:09:32 +0100

    added config path for zwave2mqtt

--- zwave2mqtt/rootfs/etc/cont-init.d/zwave2mqtt.sh
+++ zwave2mqtt/rootfs/etc/cont-init.d/zwave2mqtt.sh
@@ -7,3 +7,8 @@
 # Ensure store is persistent
 rm -f -r /opt/store
 ln -s /data /opt/store
+
+if bashio::config.exists 'config_path'; then
+    rm -f -r /usr/etc/openzwave
+    ln -s $(bashio::config 'config_path') /usr/etc/openzwave
+fi
