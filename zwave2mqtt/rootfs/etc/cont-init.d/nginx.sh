d2681c16e3e29661fcb34cf910c4fc2032234526
Franck Nijhof <frenck@addons.community>
2021-01-24 21:03:55 +0100

    Refactor NGINX configuration and HA authentication (#77)

--- zwave2mqtt/rootfs/etc/cont-init.d/nginx.sh
+++ zwave2mqtt/rootfs/etc/cont-init.d/nginx.sh
@@ -3,23 +3,12 @@
 # Home Assistant Community Add-on: Z-Wave to MQTT
 # Configures NGINX for use with this add-on.
 # ==============================================================================
-declare certfile
-declare keyfile
-declare dns_host
-
 bashio::config.require.ssl
-
-if bashio::config.true 'ssl'; then
-    certfile=$(bashio::config 'certfile')
-    keyfile=$(bashio::config 'keyfile')
-
-    mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
-    sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
-    sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
-
-else
-    mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
-fi
-
-dns_host=$(bashio::dns.host)
-sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
+bashio::var.json \
+    certfile "$(bashio::config 'certfile')" \
+    keyfile "$(bashio::config 'keyfile')" \
+    leave_front_door_open "^$(bashio::config 'leave_front_door_open')" \
+    ssl "^$(bashio::config 'ssl')" \
+    | tempio \
+        -template /etc/nginx/templates/direct.gtpl \
+        -out /etc/nginx/servers/direct.conf
